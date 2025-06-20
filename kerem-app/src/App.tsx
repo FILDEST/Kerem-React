808b0a0b03656e57d7e502879cf92f58ad4d7a53
nisan <dfildest@email.com>
2025-06-20 09:40:11 +0300

    Refactor App component to display a user list and create UserList component

--- kerem-app/src/App.tsx
+++ kerem-app/src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserList from './UserList';
 
 function App() {
+    const userNames = ["Omer", "Kerem", "Ali", "Fatma", "Mehmet"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+      <h1>Users List</h1>
+      <UserList users={userNames} />
     </div>
+
   );
 }
 
